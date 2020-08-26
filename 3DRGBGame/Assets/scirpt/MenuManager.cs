ca85beb0497617e64c62b7944012de1e117b4798
chiang8610 <67904649+chiang8610@users.noreply.github.com>
2020-08-26 21:33:16 +0800

    進度條載入

--- 3DRGBGame/Assets/scirpt/MenuManager.cs
+++ 3DRGBGame/Assets/scirpt/MenuManager.cs
@@ -53,6 +53,11 @@ public class MenuManager : MonoBehaviour
             imgLoading.fillAmount = ao.progress /0.9f ;                                                                           //載入吧條 = ao.進度
             yield return null;
 
+            if (ao.progress == 0.9f)                                                                                                             //如果 ao.進度 等於0.9
+            {
+                textTip.enabled = true;                                                                                                          //題是文字.啟動 = 是 - 顯示提是文字
+                if (Input.anyKeyDown) ao.allowSceneActivation = true;                                                   //如果按下任意鍵 允許自動載入
+            }
         
         }
     }
